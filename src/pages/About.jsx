202ddf69521b36c5f8e6005eb75a3b8c04bb76c4
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-03 10:32:07 -0500

    Added about info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Layout>
+      <h1 className="text-5xl my-6">Edwin Cacuango Cahueñas</h1>
+      <p className="max-w-[50rem] text-center mx-auto leading-7">
+        Hola¡ Déjame te cuento mi historia: mi interés en el mundo del
+        desarrollo empezó allá por el 2020 cuando me involucré en el uso de
+        herramientas web y control ciudadano. En este poco tiempo he descubierto
+        todas las ventajas y oportunidades que suponen la fusión de la
+        tecnología y el derecho y estoy realmente entusiasmado de poder
+        colaborar en la creación y distribución de dichas soluciones digitales.
+        Actualmente sigo estudiando para convertirme en un experto en desarrollo
+        web, datos personales y ciberseguridad.
+      </p>
+    </Layout>
+  );
 };
 
 export default About;
