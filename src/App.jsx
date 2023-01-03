887e4a0f4ff8956d902dc0e3677b97c94be193b2
Edwin Cacuango <edwin-fernando@hotmail.es>
2023-01-03 11:00:30 -0500

    Added page for show clock

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import IMC from "./pages/IMC";
 import Age from "./pages/Age";
+import Clock from "./pages/Clock";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/imc-calculator" element={<IMC />} />
         <Route path="/age-calculator" element={<Age />} />
+        <Route path="/clock" element={<Clock />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
---
+++ src/pages/Clock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Clock = () => {
+  return (
+    <Layout>
+      <h1>Proximamente disponible</h1>
+    </Layout>
+  );
+};
+
+export default Clock;
